cdff2069e304d160dcf88f627311a1740933992c
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2025-09-01 20:26:40 -0600

    Update car listing to be a table

--- car-challenge/src/pages/listings.tsx
+++ car-challenge/src/pages/listings.tsx
@@ -13,21 +13,32 @@ export default function Listings() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] m-2">
                 Car Listings
             </h1>
 
-            <ul>
-                {list.data?.map((listItem) => (
-                    <li key={listItem.id}>
-                        <div>ID: {listItem.id}</div>
-                        <div>Make: {listItem.make}</div>
-                        <div>Model: {listItem.model}</div>
-                        <div>Year: {listItem.year}</div>
-                        <div>Price: {listItem.price}</div>    
-                    </li>
-                ))}
-            </ul>
+            <table className="table-auto text-white border-collapse border">
+                <thead>
+                    <tr>
+                        <th className="border p-2">ID</th>
+                        <th className="border p-2">Make</th>
+                        <th className="border p-2">Model</th>
+                        <th className="border p-2">Year</th>
+                        <th className="border p-2">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.data?.map((listItem) => (
+                        <tr key={listItem.id}>
+                            <td className="border p-2">{listItem.id}</td>
+                            <td className="border p-2">{listItem.make}</td>
+                            <td className="border p-2">{listItem.model}</td>
+                            <td className="border p-2">{listItem.year}</td>
+                            <td className="border p-2">${listItem.price.toLocaleString("en-US")}</td>   
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </main>
         </>
     )
