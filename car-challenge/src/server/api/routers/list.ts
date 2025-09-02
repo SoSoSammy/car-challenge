5125f60c7c3916341725dafc97cf0f562e587d11
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2025-09-01 20:33:48 -0600

    Add comment

--- car-challenge/src/server/api/routers/list.ts
+++ car-challenge/src/server/api/routers/list.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const listRouter = createTRPCRouter({
     list: publicProcedure.query(async ({ ctx }) => {
+        // Get all the entries in the listings table
         const listings = await ctx.db.listings.findMany();
         return listings ?? null;
     }),
