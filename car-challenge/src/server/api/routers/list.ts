aa54a5f74b9f4b647c7d5d6c0d81f669822fa9c7
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2025-09-01 20:12:50 -0600

    Update listRouter to return listings from Supabase

--- car-challenge/src/server/api/routers/list.ts
+++ car-challenge/src/server/api/routers/list.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const listRouter = createTRPCRouter({
     list: publicProcedure.query(async ({ ctx }) => {
-        return 'car listing';
+        const listings = await ctx.db.listings.findMany();
+        return listings ?? null;
     }),
 });
